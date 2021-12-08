54f02572308a21dc47365732fb09df63287b74ae
Alla <prirez@bk.ru>
2021-12-08 14:13:05 +0300

    create project

---
+++ Homework16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework16
+//
+//  Created by aprirez on 12/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
