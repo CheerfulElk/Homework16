01e68c262c0f923da992b744dbdaed832c348b2d
Alla <prirez@bk.ru>
2021-12-09 13:41:56 +0300

    another title

--- Homework16/ViewController.swift
+++ Homework16/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .gray
+        titleLabel.text = "Title"
     }
 
 
